8e0054c669bfacba64f412c7a281bd3da7b1bf29
Marcela Garcés <clmarce268@gmail.com>
2026-05-06 11:43:28 -0500

    6 de mayo

--- Mision_2/chatv1/chatv1.py
+++ Mision_2/chatv1/chatv1.py
@@ -3,5 +3,8 @@ from sklearn.feature_extraction.text import CountVectorizer
 # CountVectorizer convierte texto en números (vectores)
 
 from sklearn.naive_bayes import MultinomialNB
-# MultinomialNB es un modelo de inteligencia artificial
-# que aprende relaciones entre texto y respuestas
+
+"""
+MultinomialNB modelo de inteligencia
+ artificial que aprende relaciones entre texto y respuestas
+"""
--- Mision_2/chatv1/implementacion.md
+++ Mision_2/chatv1/implementacion.md
@@ -12,3 +12,17 @@ python -c "import nltk.download ('punkt')
 
 vsual code
 python -m pip install nltk
+
+ejemplo colab
+import nltk
+nltk.download('punkt')
+
+visual code
+python -m pip install nltk
+
+python
+import nltk
+nltk.download('punkt')
+
+instalar librerias
+``pip install scikit-learn nltk numpy gym==0.26.2 gym-notices``
